9be4ac8b074f47615dd63e4a06a61aedb55e2fbc
Samantha Paez <sampaez@udel.edu>
2025-03-11 17:40:46 -0400

    all test but editMode passes

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [chosen, setChosen] = useState(options[0]);
+
+    function updateChosen(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChosen(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <div className="options">
+                <Form.Group controlId="options">
+                    <Form.Label>Multiple Choice Options</Form.Label>
+                    <Form.Select value={chosen} onChange={updateChosen}>
+                        {options.map((option: string) => (
+                            <option
+                                key={option}
+                                value={option}
+                                label={option}
+                            ></option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </div>
+            {chosen === expectedAnswer && <div>✔️</div>}
+            {chosen != expectedAnswer && <div>❌</div>}
         </div>
     );
 }
