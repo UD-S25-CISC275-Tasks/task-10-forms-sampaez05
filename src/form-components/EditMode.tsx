fcc82623284a120358a039cfc7ba5828bc80608d
Samantha Paez <sampaez@udel.edu>
2025-03-12 13:30:40 -0400

    made all tests pass

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -20,31 +20,89 @@ export function EditMode(): React.JSX.Element {
 
     return (
         <div>
-            <h3>Edit Mode</h3>
-            <div>
-                {username} is {student ? "a student" : "not a student"}
-            </div>
-            <Form.Check
-                type="switch"
-                id="mode-switch"
-                label="Edit Mode"
-                checked={mode}
-                onChange={updateMode}
-            />
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-                value={username}
-                onChange={updateUsername}
-                disabled={!mode}
-            />
-            <Form.Check
-                type="checkbox"
-                id="is-student-check"
-                label="Are You A Student"
-                checked={student}
-                onChange={updateStudent}
-                disabled={!mode}
-            />
+            {mode ?
+                <>
+                    {" "}
+                    <Form.Group>
+                        <Form.Switch
+                            controlId="form-switch"
+                            type="switch"
+                            id="mode-switch"
+                            label="Edit Mode"
+                            checked={mode}
+                            onChange={updateMode}
+                        />
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="input"
+                            value={username}
+                            onChange={updateUsername}
+                            disabled={!mode}
+                        />
+                        <Form.Label>Student Status</Form.Label>
+                        <Form.Check
+                            type="checkbox"
+                            id="is-student-check"
+                            label="Are You A Student"
+                            checked={student}
+                            onChange={updateStudent}
+                            disabled={!mode}
+                        />
+                    </Form.Group>
+                    <div>
+                        {username} is {student ? "a student" : "not a student"}
+                    </div>
+                </>
+            :   <>
+                    <div>
+                        {username} is {student ? "a student" : "not a student"}
+                    </div>
+                    <Form.Switch
+                        controlId="form-switch"
+                        type="switch"
+                        id="mode-switch"
+                        label="Edit Mode"
+                        checked={mode}
+                        onChange={updateMode}
+                    />
+                </>
+            }
         </div>
+        //     <div>
+        //         <h3>Edit Mode</h3>
+        //         <div>
+        //             {username} is {student ? "a student" : "not a student"}
+        //         </div>
+        // <Form.Group>
+        //     <Form.Switch
+        //         controlId="form-switch"
+        //         type="switch"
+        //         id="mode-switch"
+        //         label="Edit Mode"
+        //         checked={mode}
+        //         onChange={updateMode}
+        //     />
+        // </Form.Group>
+        // <Form.Group>
+        //     <Form.Label>Name</Form.Label>
+        //     <Form.Control
+        //         type="input"
+        //         value={username}
+        //         onChange={updateUsername}
+        //         disabled={!mode}
+        //     />
+        // </Form.Group>
+        // <Form.Group>
+        //     <Form.Label>Student Status</Form.Label>
+        //     <Form.Check
+        //         type="checkbox"
+        //         id="is-student-check"
+        //         label="Are You A Student"
+        //         checked={student}
+        //         onChange={updateStudent}
+        //         disabled={!mode}
+        //     />
+        // </Form.Group>
+        // </div>
     );
 }
